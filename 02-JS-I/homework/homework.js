a1346cdc0cdf7e42259dac8e09dfade7d7f9e0d8
pbolig <pedro.bolig@gmail.com>
2022-10-05 17:51:18 -0300

    HW 02-JS-1-Realizada2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -235,7 +235,7 @@ function esVocal(letra){
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
-  // Si no es vocal, tambien debe devolver "Dato incorrecto".
+  // Si no es vocal, tambien debe devolver "Dato incorrecto ".
   //Escribe tu código aquí
   const vocales = ['a', 'e', 'i', 'o', 'u']
   if (letra.length > 1 | typeof(letra) !== 'string' | !vocales.includes(letra)){
